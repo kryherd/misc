90d32efcb5a014a72b1fb1473ce6a6498c2790a0
Kayleigh Ryherd <kayleigh.ryherd@uconn.edu>
2019-04-25 16:46:39 -0400

    updated paradigm

--- HEARS/hears_skel.py
+++ HEARS/hears_skel.py
@@ -22,7 +22,6 @@ import pandas as pd
 # create dialog to enter subject information
 myDlg = gui.Dlg(title="HeARS")
 myDlg.addField('ID Number:')
-myDlg.addField('Order:')
 myDlg.addField('Computer Type:', choices=["PC", "Mac"])
 myDlg.addField('Experiment Type:', choices=["Full", "Clipped"])
 myDlg.show()#show dialog and wait for OK or Cancel
@@ -33,16 +32,8 @@ else:
 
 # create variables based on dialog
 subject = info[0]
-order = int(info[1])
-computer = info[2]
-exp_type = info[3]
-
-# If the order is not valid, throw an error
-if order < 1 or order > 2:
-    errordlg = gui.Dlg(title = "Error")
-    errordlg.addText("Invalid order selected.")
-    errordlg.show()
-    core.quit()
+computer = info[1]
+exp_type = info[2]
 
 # Mac/PC filepaths are different
 if computer == "PC":
@@ -58,9 +49,9 @@ parent_dir = "." + slash
 
 # select stim type
 if exp_type == 'Full':
-    stim_length = "stim.csv"
+    stim_length = ".csv"
 elif exp_type == 'Clipped':
-    stim_length = "stim_clip.csv"
+    stim_length = "_clip.csv"
 else:
     print("error in exp type selection")
     core.quit()
@@ -95,27 +86,16 @@ logging.setDefaultClock(globalClock)
 #info about the screen
 # change to match your resolution
 win = visual.Window(size = [1440,900],
-                    color = "black",
+                    color = "white",
                     fullscr = True, allowGUI=False,
                     units = "pix")
 
 ############################# 4AFC TASK
 
-# select block
-if order == 1:
-    block = {1: "silent", 2: "noise"}
-elif order == 2:
-    block = {1: "noise", 2: "silent"}
-else:
-    print("\n-----------\nInvalid Order Selected. Please Try Again.\n-----------\n")
-    win.close()
-    core.quit()
-
-
 # set up text windows
-instruct_txt = visual.TextStim(win, text = "HERE YOU CAN CHANGE INSTRUCTION TEXT." ,
+instruct_txt = visual.TextStim(win, text = "Today you will hear some words and see some pictures. \n \nPress the button that for the picture that matches the word you hear. \n \nPress SPACE to continue." ,
                         pos = [0.0,0.0],
-                        color = "white",
+                        color = "black",
                         height = 32,
                         alignHoriz='center',
                         alignVert='center',
@@ -124,9 +104,9 @@ instruct_txt = visual.TextStim(win, text = "HERE YOU CAN CHANGE INSTRUCTION TEXT
                         autoLog=True)
                         
 
-instruct_txt2 = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT PAGE 2." ,
+instruct_txt2 = visual.TextStim(win, text = "Use the 4 arrow keys to pick a picture. \n \nLet's practice! \n \nPress SPACE to continue." ,
                         pos = [0.0,0.0],
-                        color = "white",
+                        color = "black",
                         height = 32,
                         alignHoriz='center',
                         alignVert='center',
@@ -134,9 +114,9 @@ instruct_txt2 = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT PAGE 2." ,
                         wrapWidth= 1400,
                         autoLog=True)
 
-instruct_txt3 = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT PAGE 3." ,
+instruct_txt3 = visual.TextStim(win, text = "Now we're going to do some more. \n \nListen carefully! \n \nPress SPACE to continue." ,
                         pos = [0.0,0.0],
-                        color = "white",
+                        color = "black",
                         height = 32,
                         alignHoriz='center',
                         alignVert='center',
@@ -144,10 +124,30 @@ instruct_txt3 = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT PAGE 3." ,
                         wrapWidth= 1400,
                         autoLog=True)
 
-diff_txt = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT IN BETWEEN BLOCKS." ,
+prac_correct = visual.TextStim(win, text = "Correct!" ,
                         pos = [0.0,0.0],
-                        color = "white",
-                        height = 32,
+                        color = "black",
+                        height = 48,
+                        alignHoriz='center',
+                        alignVert='center',
+                        font = "Arial",
+                        wrapWidth= 1400,
+                        autoLog=True)
+
+prac_incorrect = visual.TextStim(win, text = "Incorrect" ,
+                        pos = [0.0,0.0],
+                        color = "black",
+                        height = 48,
+                        alignHoriz='center',
+                        alignVert='center',
+                        font = "Arial",
+                        wrapWidth= 1400,
+                        autoLog=True)
+
+prac_slow = visual.TextStim(win, text = "Too Slow" ,
+                        pos = [0.0,0.0],
+                        color = "black",
+                        height = 48,
                         alignHoriz='center',
                         alignVert='center',
                         font = "Arial",
@@ -156,125 +156,195 @@ diff_txt = visual.TextStim(win, text = "THIS IS INSTRUCTION TEXT IN BETWEEN BLOC
 
 FinalThankYou_txt = visual.TextStim(win, text = "Thank you!",
                         pos = [0.0,0.0],
-                        color = "white",
+                        color = "black",
                         height = 50,
                         alignHoriz='center',
                         alignVert='center',
                         font = "Arial",
                         autoLog=True)
 
-# set up example sounds
-ba_redu = sound.Sound(parent_dir + "audio" + slash + "ba_redu_audio.wav")
-ba_base = sound.Sound(parent_dir + "audio" + slash + "ba_base_audio.wav")
-
 # header for data log
-data = np.hstack(("Subject", "BlockNum", "BlockType", "Trial", "StimType", "Sound","CorImg", "RespImg","ACC", "RT"))
+data = np.hstack(("Subject", "StimType", "Trial", "Sound","SpeakerGender", "TargetImage", "Foil1", "Foil2", "Foil3", "CorImgLoc", "RespKey","ACC", "RT"))
 
 # set positions for images
 positions = [[0.0, -250], [0.0, 250], [-400, 0.0], [400, 0.0]]
-directions = {"[0.0, -250]": "up", "[0.0, 250]": "down", "[-400, 0.0]": "left", "[400, 0.0]": "right"}
+directions = {"[0.0, -250]": "down", "[0.0, 250]": "up", "[-400, 0.0]": "left", "[400, 0.0]": "right"}
 # create indices for randomizing image placement
 indices = [0, 1, 2, 3]
+
+# create gender
+gender = ["Male", "Female"]
 
 ##### RUN EXPERIMENT
 # time when experiment started
 t0 = globalClock.getTime()
-# run through the blocks in order
-for i in range(1, 3):
-    # for the first block, show the instructions
-#    if i == 1:
-#        instruct_txt.draw()
-#        win.flip()
-#        # when space is pressed...
-#        keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
-#        # ... play the A sound
-#        ba_redu.play()
-#        core.wait(2)
-#        instruct_txt2.draw()
-#        win.flip()
-#        # when space is pressed...
-#        keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
-#        # ... play the BA sound
-#        ba_base.play()
-#        core.wait(2)
-#        instruct_txt3.draw()
-#        win.flip()
-#        keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
-    # for the other blocks, show the other block instructions  
-#    if i > 1:
-#        diff_txt.draw()
-#        win.flip()
-#        keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
-    # name the block
-    blockName = block[i]
-    # load in trials and randomize them
-    TRIAL_LIST = psychopy.data.importConditions(fileName = parent_dir + "stim" + slash + "%s_" % (blockName) + stim_length)
-    totalTrials = len(TRIAL_LIST)
-    TRIAL_LIST_RAND = TRIAL_LIST
-    random.shuffle(TRIAL_LIST_RAND)
-    # set trial counter
-    Trial = 0
-    # run through trials
-    for index in range(len(TRIAL_LIST_RAND)):
-        # randomize location of images
-        random.shuffle(indices)
-        img1pos = positions[indices[0]]
-        img2pos = positions[indices[1]]
-        img3pos = positions[indices[2]]
-        img4pos = positions[indices[3]]
-        CorImgPos = directions[''.join(str(positions[indices[TRIAL_LIST_RAND[index]['CorrectImage']-1]]))]
-        # set up image stimuli
-        img1 = visual.ImageStim(win, pos = img1pos,
-                    size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Image1'])
-        img2 = visual.ImageStim(win, pos = img2pos,
-                    size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Image2'])
-        img3 = visual.ImageStim(win, pos = img3pos,
-                    size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Image3'])
-        img4 = visual.ImageStim(win, pos = img4pos,
-                    size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Image4'])
-        # timestamp right as images show up
-        t1 = globalClock.getTime()
-        # draw images
-        img1.draw()
-        img2.draw()
-        img3.draw()
-        img4.draw()
-        # show screen
+# show the instructions
+instruct_txt.draw()
+win.flip()
+# when space is pressed...
+keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
+# go to the next instruction
+instruct_txt2.draw()
+win.flip()
+# when space is pressed, move on to practice
+keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
+### PRACTICE TRIALS
+PRACTICE_LIST = psychopy.data.importConditions(fileName = parent_dir + "stim" + slash + "practice_stim.csv")
+PRACTICE_LIST_RAND = PRACTICE_LIST
+random.shuffle(PRACTICE_LIST_RAND)
+Trial = 0
+# run through practice trials
+for index in range(len(PRACTICE_LIST_RAND)):
+    # randomize location of images
+    random.shuffle(indices)
+    targpos = positions[indices[0]]
+    foil1pos = positions[indices[1]]
+    foil2pos = positions[indices[2]]
+    foil3pos = positions[indices[3]]
+    CorImgPos = directions[''.join(str(targpos))]
+    # set up image stimuli
+    img1 = visual.ImageStim(win, pos = targpos,
+                size = [300,300], image = parent_dir + "pictures" + slash + PRACTICE_LIST_RAND[index]['TargetImage'] + ".jpg")
+    img2 = visual.ImageStim(win, pos = foil1pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + PRACTICE_LIST_RAND[index]['Foil1'] + ".jpg")
+    img3 = visual.ImageStim(win, pos = foil2pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + PRACTICE_LIST_RAND[index]['Foil2'] + ".jpg")
+    img4 = visual.ImageStim(win, pos = foil3pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + PRACTICE_LIST_RAND[index]['Foil3'] + ".jpg")
+    # randomize gender
+    random.shuffle(gender)
+    gender_trial = gender[0]
+    # set up sound 
+    trial_sound = sound.Sound(parent_dir + "audio" + slash + PRACTICE_LIST_RAND[index]['StimType'] +  slash + gender_trial + slash + PRACTICE_LIST_RAND[index]['Sound'] + ".wav")
+    # draw images
+    img1.draw()
+    img2.draw()
+    img3.draw()
+    img4.draw()
+    # show screen
+    trial_sound.play()
+    win.flip()
+    # timestamp right as images show up
+    t1 = globalClock.getTime()
+    while globalClock.getTime() - t1 < 5:
+        KEY = get_keypress()
+        # if a key was pressed
+        if KEY != None:
+            RESP = KEY[0][0] # save key that was pressed
+            RT = KEY[0][1] - t1 # calculate RT
+            break
+        # if nothing was pressed
+    if KEY == None:
+        RESP = 'none'
+        RT = 999
+    # calculate accuracy
+    if RESP == CorImgPos:
+        ACC = 1
+        prac_correct.draw()
         win.flip()
-        while globalClock.getTime() - t1 < 10:
-            KEY = get_keypress()
-            # if a key was pressed
-            if KEY != None:
-                RESP = KEY[0][0] # save key that was pressed
-                RT = KEY[0][1] - t1 # calculate RT
-                break
-                if RESP == "up":
-                    RespImg = 1
-                elif RESP == 'j':
-                    SOUND = 'BA'
-                else:
-                    SOUND = 'none'
-            # if nothing was pressed
-        if KEY == None:
-            RESP = 'none'
-            RT = 999
-        # calculate accuracy
-        if RESP == CorImgPos:
-            ACC = 1
-        else:
-            ACC = 0
-        Trial = Trial + 1
-        # save data
-        data = np.vstack((data, np.hstack((subject,
-        i,
-        blockName,
-        Trial,
-        TRIAL_LIST_RAND[index]['StimType'],
-        TRIAL_LIST_RAND[index]['Sound'],
-        CorImgPos, 
-        RESP,
-        ACC,
-        "%.3f" %RT))))
+        core.wait(0.5)
+    elif RESP == 'none':
+        ACC = 0
+        prac_slow.draw()
+        win.flip()
+        core.wait(0.5)
+    else:
+        ACC = 0
+        prac_incorrect.draw()
+        win.flip()
+        core.wait(0.5)
+    Trial = Trial + 1
+    # save data
+    data = np.vstack((data, np.hstack((subject,
+    PRACTICE_LIST_RAND[index]['StimType'],
+    Trial,
+    PRACTICE_LIST_RAND[index]['Sound'],
+    gender_trial,
+    PRACTICE_LIST_RAND[index]['TargetImage'],
+    PRACTICE_LIST_RAND[index]['Foil1'],
+    PRACTICE_LIST_RAND[index]['Foil2'],
+    PRACTICE_LIST_RAND[index]['Foil3'],
+    CorImgPos, 
+    RESP,
+    ACC,
+    "%.3f" %RT))))
+instruct_txt3.draw()
+win.flip()
+# when space is pressed, move on to the actual task
+keys = event.waitKeys(keyList=['space'], timeStamped=globalClock)
+### EXPERIMENTAL TRIALS
+# load in trials and randomize them
+TRIAL_LIST = psychopy.data.importConditions(fileName = parent_dir + "stim" + slash + "stim_list" + stim_length)
+TRIAL_LIST_RAND = TRIAL_LIST
+random.shuffle(TRIAL_LIST_RAND)
+# set trial counter
+Trial = 0
+# run through trials
+for index in range(len(TRIAL_LIST_RAND)):
+    # randomize location of images
+    random.shuffle(indices)
+    targpos = positions[indices[0]]
+    foil1pos = positions[indices[1]]
+    foil2pos = positions[indices[2]]
+    foil3pos = positions[indices[3]]
+    CorImgPos = directions[''.join(str(targpos))]
+    # set up image stimuli
+    img1 = visual.ImageStim(win, pos = targpos,
+                size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['TargetImage'] + ".jpg")
+    img2 = visual.ImageStim(win, pos = foil1pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Foil1'] + ".jpg")
+    img3 = visual.ImageStim(win, pos = foil2pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Foil2'] + ".jpg")
+    img4 = visual.ImageStim(win, pos = foil3pos,
+                size = [300,300], image = parent_dir + "pictures" + slash + TRIAL_LIST_RAND[index]['Foil3'] + ".jpg")
+    # randomize gender
+    random.shuffle(gender)
+    gender_trial = gender[0]
+    # set up sound 
+    trial_sound = sound.Sound(parent_dir + "audio" + slash + TRIAL_LIST_RAND[index]['StimType'] + slash + gender_trial + slash + TRIAL_LIST_RAND[index]['Sound'] + ".wav")
+    # draw images
+    img1.draw()
+    img2.draw()
+    img3.draw()
+    img4.draw()
+    # show screen
+    trial_sound.play()
+    win.flip()
+    # timestamp right as images show up
+    t1 = globalClock.getTime()
+    while globalClock.getTime() - t1 < 5:
+        KEY = get_keypress()
+        # if a key was pressed
+        if KEY != None:
+            RESP = KEY[0][0] # save key that was pressed
+            RT = KEY[0][1] - t1 # calculate RT
+            break
+    # if nothing was pressed
+    if KEY == None:
+        RESP = 'none'
+        RT = 999
+    # calculate accuracy
+    if RESP == CorImgPos:
+        ACC = 1
+    else:
+        ACC = 0
+    Trial = Trial + 1
+    win.flip()
+    core.wait(0.25)
+    # save data
+    data = np.vstack((data, np.hstack((subject,
+    TRIAL_LIST_RAND[index]['StimType'],
+    Trial,
+    TRIAL_LIST_RAND[index]['Sound'],
+    gender_trial,
+    TRIAL_LIST_RAND[index]['TargetImage'],
+    TRIAL_LIST_RAND[index]['Foil1'],
+    TRIAL_LIST_RAND[index]['Foil2'],
+    TRIAL_LIST_RAND[index]['Foil3'],
+    CorImgPos, 
+    RESP,
+    ACC,
+    "%.3f" %RT))))
 
 # save results document
 np.savetxt(parent_dir + "results" + slash + "HeARS_" + prefix + ".tsv", data, fmt='%s', delimiter='\t', newline='\n', header='', footer='', comments='# ')
